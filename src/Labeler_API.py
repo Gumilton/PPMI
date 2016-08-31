e377c75531c2abb3bb61417a2d79a544a7fd89f6
SingularitySun <sjhthu@163.com>
2016-08-31 08:25:17 +0800

    update notes about input matrix

--- src/Labeler_API.py
+++ src/Labeler_API.py
@@ -6,8 +6,8 @@ Created on Wed Aug 31 02:16:22 2016
 """
 # Different Matrices with 100 feature dimensions preprocessed with differet ways will be passed to you.
 # You may subset it with the first (Because the more important the feature is, the smaller its index will be) 
-#   several features (eg. 10 or 30 or 50) locally before passing it to the Labeler.
-# If you do so, please keep it the same format as the .txt file passed to you originally.
+#   several features (eg. 10 or 30 or 50) locally before passing it to the Learner.
+
 
 def Learner(Matrix):    # Matrix is a numpy.ndarray, dtype = float, shape = (PatientNum, FeatureNum).
     import numpy as np    
